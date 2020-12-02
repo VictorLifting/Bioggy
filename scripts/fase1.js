6adc386e6b2511746d6f2582d0353a2a4dffaf8e
victor <contactovictore@gmail.com>
2020-12-02 08:11:13 -0500

    interaccion y modal

--- scripts/fase1.js
+++ scripts/fase1.js
@@ -1,10 +1,10 @@
-//const next= document.querySelector('.btn--next');
+const next= document.querySelector('.btn--next');
 const back= document.querySelector('.btn--back');
-/*
+
 next.addEventListener('click',function(){
-window.location.href="fase1.html";
+window.location.href="video2.html";
 });
-*/
+
 
 back.addEventListener('click',function(){
     window.location.href="video1.html";
--- scripts/video2.js
+++ scripts/video2.js
@@ -2,10 +2,9 @@ const next= document.querySelector('.btn--next');
 const back= document.querySelector('.btn--back');
 
 next.addEventListener('click',function(){
-window.location.href="video3.html";
+window.location.href="fase2.html";
 });
 
-
 back.addEventListener('click',function(){
-    window.location.href="video2.html";
+    window.location.href="fase1.html";
     });
