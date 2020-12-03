2d7afafc7dd3a0cafc3e031ac4dd1dc0a72695c3
victor <contactovictore@gmail.com>
2020-12-03 10:13:15 -0500

    avances es las fases

---
+++ scripts/fase3.js
@@ -0,0 +1,11 @@
+const next= document.querySelector('.btn--next');
+const back= document.querySelector('.btn--back');
+
+next.addEventListener('click',function(){
+window.location.href="video4.html";
+});
+
+
+back.addEventListener('click',function(){
+    window.location.href="video3.html";
+    });
